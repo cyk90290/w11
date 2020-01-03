f2f540d88b3176937c9df52b6bf0c28c3b1b0c57
cyk90290 <cyk87321@yahoo.com.tw>
2020-01-03 15:14:02 +0800

    Initial commit

---
+++ lib/src/main/java/com/example/lib/w11.java
@@ -0,0 +1,8 @@
+package com.example.lib;
+import java.util.Scanner;
+public class w11 {
+    public static void main(String[] args) {
+        MyClass MyClass = new MyClass("7_8.txt");
+        MyClass.Judge();
+    }
+}
